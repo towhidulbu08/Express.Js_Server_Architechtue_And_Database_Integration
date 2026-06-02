003ec750c68de2598109d7a34a456a35f97621bf
towhidulbu08 <towhidulislam26121997@gmail.com>
2026-06-02 06:24:49 +0600

    feat: initialize users table with schema definition

--- src/server.ts
+++ src/server.ts
@@ -16,6 +16,31 @@ const pool = new Pool({
     "postgresql://neondb_owner:npg_ueX6nZjw5GbW@ep-orange-tree-aqjbei2j.c-8.us-east-1.aws.neon.tech/neondb?sslmode=require",
 });
 
+const initDB = async () => {
+  try {
+    await pool.query(
+      `
+      CREATE TABLE IF NOT EXISTS users(
+      id  SERIAL PRIMARY KEY,
+      name VARCHAR(20),
+      email VARCHAR(20) NOT NULL,
+      password VARCHAR(20) NOT NULL,
+      is_active BOOLEAN DEFAULT true,
+      age INT,
+      
+      created_at TIMESTAMP DEFAULT NOW(),
+      update TIMESTAMP DEFAULT NOW()
+
+      )
+      `,
+    );
+    console.log("Database connected successfully");
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+initDB();
 app.get("/", (req: Request, res: Response) => {
   // res.send("Hello World!");
   res.status(200).json({
