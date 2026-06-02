11df62cc6d40d056346d37a1eef8b08320d8a6c1
towhidulbu08 <towhidulislam26121997@gmail.com>
2026-06-03 04:22:42 +0600

    feat:Update User with the PUT method

--- src/server.ts
+++ src/server.ts
@@ -108,17 +108,59 @@ app.get("/api/users/:id", async (req: Request, res: Response) => {
       [id],
     );
 
-    // if (result.rows.length === 0) {
-    //   res.status(500).json({
-    //     success: false,
-    //     message: "user not found",
-    //     data: {},
-    //   });
-    //   return;
-    // }
+    if (result.rows.length === 0) {
+      res.status(404).json({
+        success: false,
+        message: "user not found",
+        data: {},
+      });
+      return;
+    }
     res.status(200).json({
       success: true,
       message: "user retrived successfully",
+      data: result.rows[0],
+    });
+  } catch (error: any) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+      error,
+    });
+  }
+});
+
+app.put("/api/users/:id", async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const { name, password, age, is_active } = req.body;
+
+  // console.log("id:", id);
+  // console.log({ name, password, age, is_active });
+
+  try {
+    const { id } = req.params;
+    const { name, password, age, is_active } = req.body;
+    const result = await pool.query(
+      `
+      
+      UPDATE users
+   SET name = $1,password=$2,age=$3,is_active=$4
+    WHERE id=$5 RETURNING *
+      `,
+      [name, password, age, is_active, id],
+    );
+    console.log("result", result);
+    if (result.rows.length === 0) {
+      res.status(404).json({
+        success: false,
+        message: "user not found",
+        data: {},
+      });
+      return;
+    }
+    res.status(200).json({
+      success: true,
+      message: "user Data updated",
       data: result.rows[0],
     });
   } catch (error: any) {
