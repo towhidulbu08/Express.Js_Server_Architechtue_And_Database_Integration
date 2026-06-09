d141ae2e8b8138a7f72b9d7bb271dc4711c324d4
towhidulbu08 <towhidulislam26121997@gmail.com>
2026-06-09 08:42:20 +0600

    9-1 Understanding Express Middleware

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import express, { type Application } from "express";
+import fs from "fs";
 import { authRoute } from "./modules/auth/auth.route";
 import { profileRoute } from "./modules/profile/profile.route";
 import { userRoute } from "./modules/user/user.route";
@@ -12,5 +13,14 @@ app.use(express.urlencoded({ extended: true }));
 app.use("/api/users", userRoute);
 app.use("/api/profile", profileRoute);
 app.use("/api/auth", authRoute);
+
+app.use((req, res, next) => {
+  console.log("Method-URL-Time:", req.method, req.url, Date.now());
+  const log = `\nMethod->${req.method} Time->${Date.now()} Url ${req.url}\n`;
+  fs.appendFile("logger.txt", log, (err) => {
+    console.log(err);
+  });
+  next();
+});
 
 export default app;
